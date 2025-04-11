8c1f2c62b309b9b8e0d767f33bd98abe09bf8cf4
claessensf <97987798+claessensf@users.noreply.github.com>
2025-04-11 19:46:57 +0200

    Update startsetup.sh

--- startsetup.sh
+++ startsetup.sh
@@ -5,7 +5,7 @@ sudo apt update && sudo apt upgrade -y
 sudo apt install -y git nano net-tools
 
 # Clone and run the setup script from GitHub
-git clone https://github.com/smart-meter-reader.git /home/pi/smart-meter-reader
+git clone https://github.com/claessensf/slimmemeter.git ./smart-meter-reader
 cd smart-meter-reader
 chmod +x setup.sh
 ./setup.sh
