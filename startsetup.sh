f90f4a6999cd6ebb95b738c1a832e2d54d072f46
claessensf <97987798+claessensf@users.noreply.github.com>
2025-04-11 19:44:56 +0200

    Update startsetup.sh

--- startsetup.sh
+++ startsetup.sh
@@ -6,6 +6,6 @@ sudo apt install -y git
 
 # Clone and run the setup script from GitHub
 git clone https://github.com/your-repo/smart-meter-reader.git /home/pi/smart-meter-reader
-cd /home/pi/smart-meter-reader
+cd smart-meter-reader
 chmod +x setup.sh
 ./setup.sh
