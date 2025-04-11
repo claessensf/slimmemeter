bd25f67132325131b68e1639813c2b3a43d2d17d
claessensf <97987798+claessensf@users.noreply.github.com>
2025-04-11 20:05:24 +0200

    Update startsetup.sh

--- startsetup.sh
+++ startsetup.sh
@@ -6,6 +6,7 @@ sudo apt autoremove -y
 sudo apt install -y git nano net-tools
 
 # Clone and run the setup script from GitHub
+rm ./slimmemeter -Rf
 git clone https://github.com/claessensf/slimmemeter.git ./slimmemeter
 cd slimmemeter
 chmod +x setup.sh
